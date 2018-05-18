007d21239ab4d334f03fb67c865e29fd0ec102cb
EvanVogelbaum <naveace@gmail.com>
2018-05-18 09:23:27 -0400

    E

--- Prophet_test.py
+++ Prophet_test.py
@@ -1,15 +1,18 @@
 from fbprophet import Prophet
 import pandas as pd
 import numpy as np
-data=pd.read_csv("bitcoinData.csv")
-print(data.head())
-data=pd.DataFrame(data,None,["ds","y"])
+data=pd.read_csv("market-price.csv")
+
+data=pd.DataFrame(data, None, ["ds","y"])
 data["y"]=np.log(data["y"])
+#data["ds"]=pd.to_datetime(data["ds"])
+print(data.head())
+
 navi = Prophet()
 
 navi.fit(data)
 
-#future = navi.make_future_dataframe(periods=365)
-#print(future.tail())
-#forecast= navi.predict(future)
-#navi.plot(forecast)
+future = navi.make_future_dataframe(periods=365)
+forecast= navi.predict(future)
+print(forecast)
+navi.plot(forecast).show()
