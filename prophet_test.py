46ab27a10049cb02b6c1f30b24915385463e081a
brufbruf <alex@brufsky.org>
2018-05-21 12:30:43 -0400

    Added test

--- prophet_test.py
+++ prophet_test.py
@@ -1,38 +1,18 @@
+from fbprophet import Prophet
 import pandas as pd
 import numpy as np
-from fbprophet import Prophet
-import matplotlib.pyplot as plt
-import pickle
-import os.path
+data=pd.read_csv("market-price.csv")
 
+data=pd.DataFrame(data, None, ["ds","y"])
+data["y"]=np.log(data["y"])
+#data["ds"]=pd.to_datetime(data["ds"])
+print(data.head())
 
-m = None
-if os.path.exists('fit_prophet_coinbase.dat'):
-  model = file('fit_prophet_coinbase.dat', 'r')
-  m = pickle.load(model)
+navi = Prophet()
 
-if m is None:
-  orig_df = pd.read_csv('./data/coinbase.csv')
-  clean_df = pd.DataFrame({
-    "ds": pd.to_datetime(orig_df['Timestamp'],unit='s')[10000:][::60],
-    'y': orig_df['Close'][10000:][::60]
-  })
+navi.fit(data)
 
-  clean_df.head()
-
-  m = Prophet()
-  print "\nFitting..."
-  m.fit(clean_df)
-  print "Fit\n"
-  f = file('fit_prophet_coinbase.dat', 'w')
-  pickle.dump(m, f)
-
-future = m.make_future_dataframe(periods=10, freq='H', include_history=False)
-print future
-print "\nPredicting..."
-forecast = m.predict(future)
-print "Predicted\n"
-forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
-fig = m.plot(forecast);
-plt.show()
-
+future = navi.make_future_dataframe(periods=365)
+forecast= navi.predict(future)
+print(forecast)
+navi.plot_components(forecast)
