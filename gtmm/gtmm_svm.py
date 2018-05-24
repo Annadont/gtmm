ccb6c3874167e7af8e2c3cb5aa2fd474324e343d
brufbruf <alex@brufsky.org>
2018-05-24 09:31:47 -0400

    Added svm and test code

--- gtmm/gtmm_svm.py
+++ gtmm/gtmm_svm.py
@@ -4,7 +4,7 @@ from fbprophet import Prophet
 import pandas as pd
 import datetime
 
-def combineInputSources(prophet_trained, twitter_setiment, price_every_x_hours, curr_price, curr_time, delta_hours=6):
+def combine_input_sources(prophet_trained, twitter_setiment, price_every_x_hours, curr_price, curr_time, delta_hours=6):
 	time_to_predict = datetime.datetime(curr_time) + datetime.timedelta(hours=delta_hours)
 	future_df = pd.DataFrame({'ds':[ time_to_predict ]}).tail()
 	predicted_df = prophet_trained.predict(future_df)
@@ -16,6 +16,17 @@ def combineInputSources(prophet_trained, twitter_setiment, price_every_x_hours, 
 		  		np.array(curr_price), 
 					price_every_x_hours
 				]).T
+
+
+def calculate_y(current_price, future_price):
+	'''calculates up down or not based on a the current price and a future price'''
+	percent_increase = (future_price - current_price)/current_price
+	if (percent_increase > 0.5):
+		return 1
+	elif (percent_increase < 0.5):
+		return -1
+	else: 
+		return 0
 
 
 class GtmmSVM():
@@ -36,4 +47,13 @@ class GtmmSVM():
 
 	def get_svc(self):
 		return self.clf 
+
+
+	def test(self, predictions, y):
+		faults = 0
+		for i in range(0, len(predictions)):
+			if predictions[i] != y:
+				faults += 1
+		
+		return faults
 
--- gtmm_test.py
+++ gtmm_test.py
@@ -1,27 +1,73 @@
-from gtmm.gtmm_svm import GtmmSVM, combineInputSources
+from gtmm.gtmm_svm import GtmmSVM, combine_input_sources, calculate_y
 from gtmm.public_sentiment import PubSent
 from gtmm.current_price import CurrPrice
 from gttm.gtmm_prophet import GtmmProphet
 from datetime import datetime, timedelta
+import numpy as np
 
-def main():
+curr_price_tool = CurrPrice()
 
-  prohpet_prices = CurrPrice().get_previous_price(period=1, end_date=datetime.now())
+def make_x(curr_time):
+  prohpet_prices = curr_price_tool.get_previous_price(period=1, end_date=curr_time)
   prophet_trainer = GtmmProphet(prohpet_prices)
   prophet_trainer.train_prophet()
   prophet_trained = prophet_trainer.get_prophet()
 
   pub_sent = PubSent() ## figure out how to get data to dawson
-  pub_sent_yhat = pub_sent.predict() ## get some data in the target time
+  previous_time = curr_time - timedelta(hours=6)
+  pub_sent_X = pub_sent.makeInputData(previous_time)
+  pub_sent_y = pub_sent.calculateY(pub_sent_X, previous_time)
+  pub_sent.train(pub_sent_X, pub_sent_y)
+  
+  pub_sent_yhat = pub_sent.predict(curr_time) ## get some data in the target time
+
+  week_prices = curr_price_tool.get_previous_price(period=60, start_date=curr_time - timedelta(hours=28), end_date=curr_time)['y']
+  curr_price = curr_price_tool.get_current_price(date=curr_time)
+
+  return combine_input_sources(prophet_trained, pub_sent_yhat, week_prices, curr_price, curr_time)
 
 
-  week_prices = CurrPrice().get_previous_price(period=60, start_date=datetime.now() - timedelta(hours=28), end_date=datetime.now())['y']
-  curr_time = datetime.now()
-  curr_price = CurrPrice().get_current_price(date=datetime.now())
+def make_x_y(curr_time):
+  X = make_x(curr_time)
+  curr_price = curr_price_tool.get_current_price(date=curr_time)
+  future_price = curr_price_tool.get_current_price(date=curr_time + timedelta(hours=6))
+  y = calculate_y(curr_price, future_price)
 
-  svm_input_X = combineInputSources(prophet_trained, pub_sent_yhat, week_prices, curr_price, curr_time)
+  return X, y
+
+
+def predict(clf, curr_time):
+  X = make_x(curr_time)
+  return clf.predict(X)
+
+
+def main():
+
+  prohpet_prices = curr_price_tool.get_previous_price(period=1, end_date=datetime.now())
+  train_examples_len = len(prohpet_prices)
+
+  X_arr = []
+  y_arr = []
+  for i in range(0, train_examples_len):
+    curr_time = prohpet_prices[i]['ds']
+    X, y = make_x_y(curr_time)
+    X_arr.append(X)
+    y_arr.append(y)
+
+  X = np.array(X_arr)
+  y = np.array(y_arr)
+
+  X_train = X[:-20]
+  y_train = X[:-20]
+
+  X_test = X[-20:]
+  y_test = X[-20:]
 
   clf = GtmmSVM()
-  clf.train(svm_input_X, ) ## figure out how to calculate y
+  clf.train(X_train, y_train) ## figure out how to calculate y
+  
+  predictions = clf.predict(X_test)
+  faults = clf.test(predictions, y_test)
+  print "Number incorrect: " + str(faults/len(X_test))
 
 main()
